9881296921491046404a3cbfd2d4b7d17558e6fb
Eduardo Nagano <eduardo_nagano@yahoo.com.br>
2024-12-26 17:12:40 -0300

    011

--- resources/views/operations.blade.php
+++ resources/views/operations.blade.php
@@ -20,28 +20,24 @@
         <img src="{{ asset('assets/images/logo.jpg') }}" alt="logo" class="img-fluid" width="250px">
     </div>
 
-
     <!-- operations -->
     <div class="container">
 
         <hr>
 
         <div class="row">
-
             @foreach ($exercises as $exercise)
                 <div class="col-3 display-6 mb-3">
-                    <span class="badge bg-dark">{{ str_pad($exercise['exercise_number'], 2, '0', STR_PAD_LEFT) }}</span>
+                    <span class="badge bg-dark">{{ $exercise['exercise_number'] }}</span>
                     <span>{{ $exercise['exercise'] }}</span>
                 </div>
             @endforeach
-
         </div>
 
         <hr>
 
     </div>
 
-    <!-- print version -->
     <div class="container mt-5">
         <div class="row">
             <div class="col">
